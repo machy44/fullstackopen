37ba45ce76ee11cb0c1b139d75360d42c3e9d245
alen.sljivar <alen.sljivar@five.agency>
2021-09-27 21:50:19 +0200

    finish 2.13

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,5 +1,77 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const API_DOMAIN = "http://api.weatherstack.com/";
+
+const fetchCountries = async (resolveFn) => {
+  const response = await axios.get("https://restcountries.com/v3/all");
+  resolveFn(response.data);
+};
+
 function App() {
-  return <div>countries</div>;
+  const [countries, setCountries] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    fetchCountries(setCountries);
+  }, []);
+
+  const searchedCountries = searchTerm
+    ? countries.filter((country) => {
+        return country.name.common
+          .toLowerCase()
+          .includes(searchTerm.toLowerCase());
+      })
+    : countries;
+
+  console.log(searchedCountries);
+
+  return (
+    <div>
+      <span>find countries: </span>
+      <input
+        type="text"
+        placeholder="Search countries by name.."
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+      {searchedCountries.length > 10 ? (
+        <div>Too many matches, specify another filter</div>
+      ) : searchedCountries.length === 1 ? (
+        <>
+          <h2>{searchedCountries[0].name.common}</h2>
+          <p>capital {searchedCountries[0].capital[0]}</p>
+          {/* there is no population key in v3 */}
+          {/* <p>population {searchedCountries[0].population}</p> */}
+          <h2>languages</h2>
+          <ul>
+            {Object.values(searchedCountries[0].languages).map((language) => {
+              return <li>{language}</li>;
+            })}
+          </ul>
+          <img
+            height="100"
+            width="100"
+            alt="country flag"
+            src={searchedCountries[0].flags[1]}
+          />
+        </>
+      ) : (
+        <ul>
+          {searchedCountries.map((country) => {
+            return (
+              <li>
+                {country.name.common}
+                <button onClick={() => setSearchTerm(country.name.common)}>
+                  show
+                </button>
+              </li>
+            );
+          })}
+        </ul>
+      )}
+    </div>
+  );
 }
 
 export default App;
