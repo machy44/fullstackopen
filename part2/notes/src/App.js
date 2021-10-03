13495c697705ca2929f22eddb3b75c273145b3a8
alen.sljivar <alen.sljivar@five.agency>
2021-10-03 18:28:11 +0200

    [part2-notes]create services

--- part2/notes/src/App.js
+++ part2/notes/src/App.js
@@ -1,11 +1,17 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
 import { Note } from "./components/Note";
+import noteService from "./services/notes";
 
 const App = () => {
   const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState("a new note...");
   const [showAll, setShowAll] = useState(true);
+
+  useEffect(() => {
+    noteService.getAll().then((initialNotes) => {
+      setNotes(initialNotes);
+    });
+  }, []);
 
   const addNote = (event) => {
     event.preventDefault();
@@ -13,30 +19,31 @@ const App = () => {
       content: newNote,
       date: new Date().toISOString(),
       important: Math.random() < 0.5,
-      id: notes.length + 1,
     };
 
-    setNotes(notes.concat(noteObject));
-    setNewNote("");
+    noteService.create(noteObject).then((returnedNote) => {
+      setNotes(notes.concat(returnedNote));
+      setNewNote("");
+    });
+  };
+
+  const toggleImportanceOf = (id) => {
+    const note = notes.find((n) => n.id === id);
+    const changedNote = { ...note, important: !note.important };
+
+    noteService.update(id, changedNote).then((returnedNote) => {
+      setNotes(
+        notes.map((note) => {
+          return note.id !== id ? note : returnedNote;
+        })
+      );
+    });
   };
 
   const handleNoteChange = (event) => {
     console.log(event.target.value);
     setNewNote(event.target.value);
   };
-
-  useEffect(() => {
-    console.log("effect");
-
-    const eventHandler = (response) => {
-      console.log("promise fulfilled");
-      setNotes(response.data);
-    };
-
-    const promise = axios.get("http://localhost:3001/notes");
-    promise.then(eventHandler);
-  }, []);
-  console.log("render", notes.length, "notes");
 
   const notesToShow = showAll ? notes : notes.filter((note) => note.important);
 
@@ -50,7 +57,11 @@ const App = () => {
       </div>
       <ul>
         {notesToShow.map((note) => (
-          <Note key={note.id} note={note} />
+          <Note
+            key={note.id}
+            note={note}
+            toggleImportance={() => toggleImportanceOf(note.id)}
+          />
         ))}
       </ul>
       <form onSubmit={addNote}>
--- part2/notes/src/components/Note.js
+++ part2/notes/src/components/Note.js
@@ -1,5 +1,12 @@
 import React from "react";
 
-export const Note = ({ note }) => {
-  return <li>{note.content}</li>;
+export const Note = ({ note, toggleImportance }) => {
+  const label = note.important ? "make not important" : "make important";
+
+  return (
+    <li>
+      {note.content}
+      <button onClick={toggleImportance}>{label}</button>
+    </li>
+  );
 };
