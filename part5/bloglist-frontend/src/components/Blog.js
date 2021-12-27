05afc70103bc85638c5ae6b23c3e9bbae6778114
alen.sljivar <alen.sljivar@five.agency>
2021-12-27 17:39:34 +0100

    finish 5.7 exercise

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,52 @@
-import React from 'react';
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-);
+import React, { useState } from 'react';
+
+const BlogMainInfo = ({ blog }) => {
+  return (
+    <span>
+      {blog.title} {blog.author}
+    </span>
+  );
+};
+const BlogDetails = ({ blog }) => {
+  return (
+    <>
+      <div>{blog.url}</div>
+      <div>
+        likes {blog.likes}
+        <button onClick={() => console.log('like')}>like</button>
+      </div>
+      <div>{blog.user.username}</div>
+    </>
+  );
+};
+
+const blogStyle = {
+  paddingTop: 10,
+  paddingLeft: 2,
+  border: 'solid',
+  borderWidth: 1,
+  marginBottom: 5,
+};
+
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false);
+
+  const toggleVisibility = () => {
+    setVisible(!visible);
+  };
+
+  return (
+    <div style={blogStyle}>
+      <BlogMainInfo blog={blog} />
+      {!visible && <button onClick={toggleVisibility}>view</button>}
+      {visible && (
+        <>
+          <button onClick={toggleVisibility}>hide</button>
+          <BlogDetails blog={blog} />
+        </>
+      )}
+    </div>
+  );
+};
 
 export default Blog;
