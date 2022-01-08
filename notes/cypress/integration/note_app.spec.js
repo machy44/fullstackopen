d391e4f0b69022fed12f93eb56a5eaca17639be2
alen.sljivar <alen.sljivar@five.agency>
2022-01-08 22:56:54 +0100

    login test fail

--- notes/cypress/integration/note_app.spec.js
+++ notes/cypress/integration/note_app.spec.js
@@ -20,6 +20,18 @@ describe('Note app', function() {
     cy.get('#login-button').click();
     cy.contains('Matti Luukkainen logged-in');
   });
+  it.only('login fails with wrong password', function() {
+    cy.contains('log in').click();
+    cy.get('#username').type('mluukkai');
+    cy.get('#password').type('wrong');
+    cy.get('#login-button').click();
+
+    cy.get('.error').should('contain', 'Wrong credentials')
+      .and('have.css', 'color', 'rgb(255, 0, 0)')
+      .and('have.css', 'border-style', 'solid');
+
+    cy.get('html').should('not.contain', 'Matti Luukkainen logged in');
+  });
   describe('when logged in', function() {
     beforeEach(function() {
       cy.contains('log in').click();
