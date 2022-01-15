92fe68deeec763ab5320858f856578861b28dc37
alen.sljivar <alen.sljivar@five.agency>
2022-01-15 13:54:04 +0100

    finish 5.17 exercise

---
+++ part5/bloglist-frontend/cypress/integration/blog-list-app.spec.js
@@ -0,0 +1,9 @@
+describe('blog-list app', function() {
+  beforeEach(function() {
+    cy.request('POST', 'http://localhost:3001/api/testing/reset');
+    cy.visit('http://localhost:3000');
+  });
+  it('Login form is shown', function() {
+    cy.contains('login to application');
+  });
+});
