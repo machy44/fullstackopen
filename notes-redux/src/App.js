54a51381414278e506876d5470e1d3d6b19cf4b2
alen.sljivar <alen.sljivar@five.agency>
2022-01-22 18:21:33 +0100

    move actions outside components

--- notes-redux/src/App.js
+++ notes-redux/src/App.js
@@ -22,18 +22,57 @@ store.dispatch({
   }
 })
 
- const App = () => {
-  return(
-    <div>
-      <ul>
-        {store.getState().map(note=>
-          <li key={note.id}>
-            {note.content} <strong>{note.important ? 'important' : ''}</strong>
-          </li>
-        )}
-        </ul>
-    </div>
-  )
+const createNote = (content) => {
+  return {
+    type: 'NEW_NOTE',
+    data: {
+      content,
+      important: false,
+      id: generateId()
+    }
+  }
+}
+
+const toggleImportanceOf = (id) => {
+  return {
+    type: 'TOGGLE_IMPORTANCE',
+    data: { id }
+  }
+}
+
+const generateId = () =>
+Math.floor(Math.random() * 1000000)
+
+const App = () => {
+const addNote = (event) => {
+  event.preventDefault()
+  const content = event.target.note.value
+  event.target.note.value = ''
+  store.dispatch(createNote(content))
+}
+
+const toggleImportance = (id) => {
+  store.dispatch(toggleImportanceOf(id))
+}
+
+return (
+  <div>
+    <form onSubmit={addNote}>
+      <input name="note" /> 
+      <button type="submit">add</button>
+    </form>
+    <ul>
+      {store.getState().map(note =>
+        <li
+          key={note.id} 
+          onClick={() => toggleImportance(note.id)}
+        >
+          {note.content} <strong>{note.important ? 'important' : ''}</strong>
+        </li>
+      )}
+    </ul>
+  </div>
+)
 }
 
 export default App;
