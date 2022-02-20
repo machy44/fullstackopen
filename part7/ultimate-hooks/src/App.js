8ed3a9b1011de7d8b3959d01a33cfb805fcd0fa9
alen.sljivar <alen.sljivar@five.agency>
2022-02-20 21:03:43 +0100

    finish 7.8 exercise

--- part7/ultimate-hooks/src/App.js
+++ part7/ultimate-hooks/src/App.js
@@ -18,10 +18,18 @@ const useField = (type) => {
 const useResource = (baseUrl) => {
   const [resources, setResources] = useState([])
 
-  // ...
+  useEffect(() => {
+    const getAll = async () => {
+      const response = await axios.get(baseUrl)
+      setResources(response.data)
+    }
+    getAll();
+  }, [baseUrl])
+  
 
-  const create = (resource) => {
-    // ...
+  const create = async (resource) => {
+      const response = await axios.post(baseUrl, resource)
+      setResources([...resources, response.data ])
   }
 
   const service = {
