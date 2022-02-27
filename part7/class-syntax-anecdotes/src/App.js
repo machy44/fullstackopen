48526d46961c71cf3ae01ddf58b018751f0488fc
alen.sljivar <alen.sljivar@five.agency>
2022-02-27 22:10:28 +0100

    fetch data from json-server

--- part7/class-syntax-anecdotes/src/App.js
+++ part7/class-syntax-anecdotes/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 
 class App extends React.Component {
   constructor(props) {
@@ -6,6 +7,17 @@ class App extends React.Component {
 
     this.state = { anecdotes: [], current: 0 };
   }
+
+  componentDidMount = () => {
+    axios.get('http://localhost:3001/anecdotes').then((response) => {
+      this.setState({ anecdotes: response.data });
+    });
+  };
+
+  handleClick = () => {
+    const current = Math.floor(Math.random() * this.state.anecdotes.length);
+    this.setState({ current });
+  };
 
   render() {
     if (this.state.anecdotes.length === 0) {
@@ -16,6 +28,7 @@ class App extends React.Component {
       <div className="container">
         <h1>anecdote of the day</h1>
         <div>{this.state.anecdotes[this.state.current].content} </div>
+        <button onClick={this.handleClick}>next</button>
       </div>
     );
   }
