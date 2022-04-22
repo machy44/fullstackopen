146bcbcbc9968db84cfe5b70f9c789375ba17587
alen.sljivar <alen.sljivar@five.agency>
2022-04-22 11:50:52 +0200

    change NavLink logic

--- part7/bloglist-frontend-redux/src/components/Navigation.js
+++ part7/bloglist-frontend-redux/src/components/Navigation.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import { Button, Text, Flex, HStack, Link as CLink, Spacer, useColorMode, useColorModeValue } from '@chakra-ui/react';
+import PropTypes from 'prop-types';
 import { useResolvedPath, useMatch } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import { MoonIcon } from '@chakra-ui/icons';
 
-function CustomLink({ text, to, ...props }) {
+function NavLink({ text, to, ...props }) {
   let resolved = useResolvedPath(to);
-  let match = useMatch({ path: resolved.pathname, end: true });
-
-  console.log({ resolved });
-  console.log({ match });
+  let match = useMatch({ path: `${resolved.pathname}/*`, end: true });
 
   return (
     <CLink as={Link} to={to} {...props} color={match ? 'teal.500' : ''}>
@@ -18,14 +16,20 @@ function CustomLink({ text, to, ...props }) {
   );
 }
 
+NavLink.propTypes = {
+  text: PropTypes.string.isRequired,
+  to: PropTypes.string.isRequired,
+  props: PropTypes.object
+};
+
 export const Navigation = ({ userName, handleClick }) => {
   const { toggleColorMode } = useColorMode();
   const bgColor = useColorModeValue('blue.100', 'whiteAlpha.100');
   return (
     <Flex p={5} background={bgColor} borderRadius="base">
       <HStack spacing={4}>
-        <CustomLink to="/blogs" text="blogs" />
-        <CustomLink to="/users" text="users" />
+        <NavLink to="/blogs" text="blogs" />
+        <NavLink to="/users" text="users" />
       </HStack>
       <Spacer />
       <HStack spacing={4}>
