06d5cac54878f43660facbdf857a19107733c673
alen.sljivar <alen.sljivar@five.agency>
2022-04-23 22:25:31 +0200

    install firebase and create firebase module

---
+++ part7/bloglist-frontend-redux/src/firebase/firebaseConfig.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-undef */
+export const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER,
+  appId: process.env.REACT_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
+};
