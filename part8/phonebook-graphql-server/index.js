d9d6d4902e51b5f518f30868ce8e269e0b59b2f5
alen.sljivar <alen.sljivar@five.agency>
2022-06-11 13:47:11 +0200

    add filtering to allPersons

--- part8/phonebook-graphql-server/index.js
+++ part8/phonebook-graphql-server/index.js
@@ -36,10 +36,14 @@ const typeDefs = gql`
     address: Address!
     id: ID!
   }
+  enum YesNo {
+    YES
+    NO
+  }
 
   type Query {
     personCount: Int!
-    allPersons: [Person!]!
+    allPersons(phone: YesNo): [Person!]!
     findPerson(name: String!): Person
   }
 
@@ -57,7 +61,14 @@ const typeDefs = gql`
 const resolvers = {
   Query: {
     personCount: () => persons.length,
-    allPersons: () => persons,
+    allPersons: (root, args) => {
+      if (!args.phone) {
+        return persons;
+      }
+      const byPhone = (person) =>
+        args.phone === 'YES' ? person.phone : !person.phone;
+      return persons.filter(byPhone);
+    },
     // The second parameter, args, contains the parameters of the query
     findPerson: (root, args) => persons.find((p) => p.name === args.name),
   },
