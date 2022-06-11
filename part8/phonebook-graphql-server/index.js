7f1f6b0fb0061c43876d5c4562b60ad3a2ad92a9
alen.sljivar <alen.sljivar@five.agency>
2022-06-11 12:45:26 +0200

    add scripts and .gitignore

--- part8/phonebook-graphql-server/index.js
+++ part8/phonebook-graphql-server/index.js
@@ -1,34 +1,33 @@
-const { ApolloServer, gql } = require('apollo-server')
+const { ApolloServer, gql, UserInputError } = require('apollo-server');
+const { v1: uuid } = require('uuid');
 
 let persons = [
   {
-    name: "Arto Hellas",
-    phone: "040-123543",
-    street: "Tapiolankatu 5 A",
-    city: "Espoo",
-    id: "3d594650-3436-11e9-bc57-8b80ba54c431"
+    name: 'Arto Hellas',
+    phone: '040-123543',
+    street: 'Tapiolankatu 5 A',
+    city: 'Espoo',
+    id: '3d594650-3436-11e9-bc57-8b80ba54c431',
   },
   {
-    name: "Matti Luukkainen",
-    phone: "040-432342",
-    street: "Malminkaari 10 A",
-    city: "Helsinki",
-    id: '3d599470-3436-11e9-bc57-8b80ba54c431'
+    name: 'Matti Luukkainen',
+    phone: '040-432342',
+    street: 'Malminkaari 10 A',
+    city: 'Helsinki',
+    id: '3d599470-3436-11e9-bc57-8b80ba54c431',
   },
   {
-    name: "Venla Ruuska",
-    street: "Nallemäentie 22 C",
-    city: "Helsinki",
-    id: '3d599471-3436-11e9-bc57-8b80ba54c431'
+    name: 'Venla Ruuska',
+    street: 'Nallemäentie 22 C',
+    city: 'Helsinki',
+    id: '3d599471-3436-11e9-bc57-8b80ba54c431',
   },
-]
-
-
+];
 
 const typeDefs = gql`
   type Address {
     street: String!
-    city: String! 
+    city: String!
   }
 
   type Person {
@@ -43,33 +42,54 @@ const typeDefs = gql`
     allPersons: [Person!]!
     findPerson(name: String!): Person
   }
-`
+
+  type Mutation {
+    # Value for the field id is not given as a parameter. Generating an id is better left for the server.
+    addPerson(
+      name: String!
+      phone: String!
+      street: String!
+      city: String
+    ): Person
+  }
+`;
 
 const resolvers = {
   Query: {
     personCount: () => persons.length,
     allPersons: () => persons,
     // The second parameter, args, contains the parameters of the query
-    findPerson: (root, args) =>
-      persons.find(p => p.name === args.name)
+    findPerson: (root, args) => persons.find((p) => p.name === args.name),
   },
-  // The parameter root of the resolver function is the person-object, 
-  // so the street and the city of the address can be taken from its fields. 
+  // The parameter root of the resolver function is the person-object,
+  // so the street and the city of the address can be taken from its fields.
   Person: {
-    address: root => {
+    address: (root) => {
       return {
         street: root.street,
-        city: root.city
+        city: root.city,
+      };
+    },
+  },
+  Mutation: {
+    addPerson: (root, args) => {
+      if (persons.find((p) => p.name === args.name)) {
+        throw new UserInputError('Name must be unique', {
+          invalidArgs: args.name,
+        });
       }
-    }
-  }
-}
+      const person = { ...args, id: uuid() };
+      persons = persons.concat(person);
+      return person;
+    },
+  },
+};
 
 const server = new ApolloServer({
   typeDefs, // graphql scheme
   resolvers, // defined queries
-})
+});
 
 server.listen().then(({ url }) => {
-  console.log(`Server ready at ${url}`)
-})
+  console.log(`Server ready at ${url}`);
+});
