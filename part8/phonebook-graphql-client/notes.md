981b0c73683e9b87c48fa3c83c33b366ba613065
alen.sljivar <alen.sljivar@five.agency>
2022-06-25 20:22:29 +0200

    setup babel and typescript

---
+++ part8/phonebook-graphql-client/notes.md
@@ -0,0 +1,12 @@
+### prettier
+
+```json
+{
+  "semi": true, // add semicolons
+  "tabWidth": 2,
+  "printWidth": 100,
+  "singleQuote": true,
+  "trailingComma": "none", // extra comma not needed in last value of object
+  "jsxBracketSameLine": true //  https://prettier.io/docs/en/options.html#deprecated-jsx-brackets
+}
+```
--- part8/phonebook-graphql-client/webpack.config.js
+++ part8/phonebook-graphql-client/webpack.config.js
@@ -1,37 +1,28 @@
 const path = require('path');
 
 const config = {
-  entry: './src/index.js',
+  entry: './src/index.tsx',
   output: {
     path: path.resolve(__dirname, 'build'),
-    filename: 'main.js'
+    filename: 'main.js',
+  },
+  resolve: {
+    // Enable webpack find ts and tsx files without an extension
+    extensions: ['.tsx', '.ts', '.jsx', '.js'],
   },
   module: {
     rules: [
       {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        options: {
-          // @preset-react -> compiles react code into ES5 code
-          // @babel/core -> transform to target env (ES6 to ES5 for example)
-          // @babel/preset-env ->which syntax transforms(and optionally, browser polyfills) are needed
-          // by your target envs. You can setup what browsers to support
-          // @babel/preset-typescript -> typescript compiler is capable of transpiling your code
-          // but it is not as flexible, powerful and complete as Babel
-          presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript']
-        }
-      },
-      {
-        test: /\.(ts|tsx)$/,
+        test: /\.tsx?$/,
         exclude: /node_modules/,
-        use: ['ts-loader']
+        use: ['babel-loader'],
       },
       // css-loader loads css files
       // style-loader Inject CSS into the DOM
-      { test: /\.css$/, use: ['style-loader', 'css-loader'] }
-    ]
+      { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+    ],
   },
   devServer: { static: path.resolve(__dirname, 'build'), compress: true, port: 3000 },
-  devtool: 'source-map'
+  devtool: 'source-map',
 };
 module.exports = config;
