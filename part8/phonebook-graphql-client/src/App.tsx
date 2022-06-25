0f2753faf52f9662a20ff2129d9652a9067bbdf3
alen.sljivar <alen.sljivar@five.agency>
2022-06-25 22:48:17 +0200

    add apollo client and fetch data

--- part8/phonebook-graphql-client/src/App.tsx
+++ part8/phonebook-graphql-client/src/App.tsx
@@ -1,21 +1,41 @@
-import React, { useState } from 'react';
+import { gql, useQuery } from '@apollo/client';
+import React from 'react';
 import './index.css';
 
+interface Address {
+  street: string;
+  city: string;
+}
+
+interface Person {
+  id: number;
+  name: string;
+  phone: number;
+  address: Address;
+}
+
+interface PersonData {
+  allPersons: Person[];
+}
+
+const ALL_PERSONS = gql`
+  query {
+    allPersons {
+      name
+      phone
+      id
+    }
+  }
+`;
+
 const App = () => {
-  const [counter, setCounter] = useState(0);
-  const [values, setValues] = useState<number[]>([]);
+  const { loading, data } = useQuery<PersonData>(ALL_PERSONS);
 
-  const handleClick = () => {
-    setCounter(counter + 1);
-    setValues(values.concat(counter));
-  };
+  if (loading) {
+    return <div>loading...</div>;
+  }
 
-  return (
-    <div className="container">
-      hello webpack {counter} clicks
-      <button onClick={handleClick}>press</button>
-    </div>
-  );
+  return <div>{data?.allPersons.map((p) => p.name).join(', ')}</div>;
 };
 
 export default App;
--- part8/phonebook-graphql-client/src/index.tsx
+++ part8/phonebook-graphql-client/src/index.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { ApolloClient, HttpLink, InMemoryCache, ApolloProvider } from '@apollo/client';
 
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: new HttpLink({
+    uri: 'http://localhost:4000',
+  }),
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root'),
+);
