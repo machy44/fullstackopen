7ec9a088d7097e18a795ac5d2958f192f4078180
alen.sljivar <alen.sljivar@five.agency>
2022-06-26 14:27:49 +0200

    add typescript to phonebook-grapqhl-server

---
+++ part8/phonebook-graphql-server/dist/index.js
@@ -0,0 +1,103 @@
+"use strict";
+const { ApolloServer, gql, UserInputError } = require('apollo-server');
+const { v1: uuid } = require('uuid');
+let persons = [
+    {
+        name: 'Arto Hellas',
+        phone: '040-123543',
+        street: 'Tapiolankatu 5 A',
+        city: 'Espoo',
+        id: '3d594650-3436-11e9-bc57-8b80ba54c431',
+    },
+    {
+        name: 'Matti Luukkainen',
+        phone: '040-432342',
+        street: 'Malminkaari 10 A',
+        city: 'Helsinki',
+        id: '3d599470-3436-11e9-bc57-8b80ba54c431',
+    },
+    {
+        name: 'Venla Ruuska',
+        street: 'Nallemäentie 22 C',
+        city: 'Helsinki',
+        id: '3d599471-3436-11e9-bc57-8b80ba54c431',
+    },
+];
+const typeDefs = gql `
+  type Address {
+    street: String!
+    city: String!
+  }
+
+  type Person {
+    name: String!
+    phone: String
+    address: Address!
+    id: ID!
+  }
+  enum YesNo {
+    YES
+    NO
+  }
+
+  type Query {
+    personCount: Int!
+    allPersons(phone: YesNo): [Person!]!
+    findPerson(name: String!): Person
+  }
+
+  type Mutation {
+    # Value for the field id is not given as a parameter. Generating an id is better left for the server.
+    addPerson(name: String!, phone: String!, street: String!, city: String): Person
+    editNumber(name: String!, phone: String!): Person
+  }
+`;
+const resolvers = {
+    Query: {
+        personCount: () => persons.length,
+        allPersons: (root, args) => {
+            if (!args.phone) {
+                return persons;
+            }
+            const byPhone = (person) => (args.phone === 'YES' ? person.phone : !person.phone);
+            return persons.filter(byPhone);
+        },
+        findPerson: (root, args) => persons.find((p) => p.name === args.name),
+    },
+    Person: {
+        address: (root) => {
+            return {
+                street: root.street,
+                city: root.city,
+            };
+        },
+    },
+    Mutation: {
+        addPerson: (root, args) => {
+            if (persons.find((p) => p.name === args.name)) {
+                throw new UserInputError('Name must be unique', {
+                    invalidArgs: args.name,
+                });
+            }
+            const person = Object.assign(Object.assign({}, args), { id: uuid() });
+            persons = persons.concat(person);
+            return person;
+        },
+        editNumber: (root, args) => {
+            const person = persons.find((p) => p.name === args.name);
+            if (!person) {
+                return null;
+            }
+            const updatedPerson = Object.assign(Object.assign({}, person), { phone: args.phone });
+            persons = persons.map((p) => (p.name === args.name ? updatedPerson : p));
+            return updatedPerson;
+        },
+    },
+};
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+});
+server.listen().then(({ url }) => {
+    console.log(`Server ready at ${url}`);
+});
--- part8/phonebook-graphql-server/src/index.ts
+++ part8/phonebook-graphql-server/src/index.ts
@@ -1,28 +1,17 @@
-const { ApolloServer, gql, UserInputError } = require('apollo-server');
-const { v1: uuid } = require('uuid');
+import { ApolloServer, gql, UserInputError } from 'apollo-server';
+import { v1 as uuid } from 'uuid';
+import { personsData, IPerson } from './data';
 
-let persons = [
-  {
-    name: 'Arto Hellas',
-    phone: '040-123543',
-    street: 'Tapiolankatu 5 A',
-    city: 'Espoo',
-    id: '3d594650-3436-11e9-bc57-8b80ba54c431',
-  },
-  {
-    name: 'Matti Luukkainen',
-    phone: '040-432342',
-    street: 'Malminkaari 10 A',
-    city: 'Helsinki',
-    id: '3d599470-3436-11e9-bc57-8b80ba54c431',
-  },
-  {
-    name: 'Venla Ruuska',
-    street: 'Nallemäentie 22 C',
-    city: 'Helsinki',
-    id: '3d599471-3436-11e9-bc57-8b80ba54c431',
-  },
-];
+interface Address {
+  street: string;
+  city: string;
+}
+
+let persons = Object.assign({}, personsData);
+
+interface PersonMutation extends Omit<IPerson, 'street' | 'city'> {
+  address: Address;
+}
 
 const typeDefs = gql`
   type Address {
@@ -49,12 +38,7 @@ const typeDefs = gql`
 
   type Mutation {
     # Value for the field id is not given as a parameter. Generating an id is better left for the server.
-    addPerson(
-      name: String!
-      phone: String!
-      street: String!
-      city: String
-    ): Person
+    addPerson(name: String!, phone: String!, street: String!, city: String): Person
     editNumber(name: String!, phone: String!): Person
   }
 `;
@@ -62,16 +46,15 @@ const typeDefs = gql`
 const resolvers = {
   Query: {
     personCount: () => persons.length,
-    allPersons: (root, args) => {
+    allPersons: (root, args: Pick<IPerson, 'phone'>) => {
       if (!args.phone) {
         return persons;
       }
-      const byPhone = (person) =>
-        args.phone === 'YES' ? person.phone : !person.phone;
+      const byPhone = (person: IPerson) => (args.phone === 'YES' ? person.phone : !person.phone);
       return persons.filter(byPhone);
     },
     // The second parameter, args, contains the parameters of the query
-    findPerson: (root, args) => persons.find((p) => p.name === args.name),
+    findPerson: (root, args: Pick<IPerson, 'name'>) => persons.find((p) => p.name === args.name),
   },
   // The parameter root of the resolver function is the person-object,
   // so the street and the city of the address can be taken from its fields.
@@ -84,13 +67,13 @@ const resolvers = {
     },
   },
   Mutation: {
-    addPerson: (root, args) => {
+    addPerson: (root, args: Omit<IPerson, 'id'>) => {
       if (persons.find((p) => p.name === args.name)) {
         throw new UserInputError('Name must be unique', {
           invalidArgs: args.name,
         });
       }
-      const person = { ...args, id: uuid() };
+      const person: IPerson = { ...args, id: uuid() };
       persons = persons.concat(person);
       return person;
     },
