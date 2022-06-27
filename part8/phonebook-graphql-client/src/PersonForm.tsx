6b3e23b19051c072953db7f2551cf8228dafb63c
alen.sljivar <alen.sljivar@five.agency>
2022-06-28 00:10:24 +0200

    useReducer instead of useState

--- part8/phonebook-graphql-client/src/PersonForm.tsx
+++ part8/phonebook-graphql-client/src/PersonForm.tsx
@@ -1,12 +1,39 @@
-import React, { useState } from 'react';
+import React, { useReducer, useState } from 'react';
 import { useMutation } from '@apollo/client';
 import { ALL_PERSONS, CREATE_PERSON } from './queries';
 
+const initialState = {
+  name: '',
+  phone: '',
+  street: '',
+  city: '',
+};
+
+// @ts-ignore
+function reducer(state, action) {
+  switch (action.type) {
+    case 'name': {
+      return { ...state, name: action.payload };
+    }
+    case 'phone': {
+      return { ...state, phone: action.payload };
+    }
+    case 'street': {
+      return { ...state, street: action.payload };
+    }
+    case 'city': {
+      return { ...state, city: action.payload };
+    }
+    case 'reset': {
+      return initialState;
+    }
+  }
+}
+
 export const PersonForm = () => {
-  const [name, setName] = useState('');
-  const [phone, setPhone] = useState('');
-  const [street, setStreet] = useState('');
-  const [city, setCity] = useState('');
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const { name, phone, street, city } = state;
 
   const [createPerson] = useMutation(CREATE_PERSON, {
     refetchQueries: [{ query: ALL_PERSONS }],
@@ -16,10 +43,7 @@ export const PersonForm = () => {
     event.preventDefault();
 
     createPerson({ variables: { name, phone, street, city } });
-    setName('');
-    setPhone('');
-    setStreet('');
-    setCity('');
+    dispatch({ type: 'reset' });
   };
 
   return (
@@ -27,16 +51,32 @@ export const PersonForm = () => {
       <h2>create new</h2>
       <form onSubmit={submit}>
         <div>
-          name <input value={name} onChange={({ target }) => setName(target.value)} />
+          name{' '}
+          <input
+            value={name}
+            onChange={({ target }) => dispatch({ type: 'name', payload: target.value })}
+          />
         </div>
         <div>
-          phone <input value={phone} onChange={({ target }) => setPhone(target.value)} />
+          phone{' '}
+          <input
+            value={phone}
+            onChange={({ target }) => dispatch({ type: 'phone', payload: target.value })}
+          />
         </div>
         <div>
-          street <input value={street} onChange={({ target }) => setStreet(target.value)} />
+          street
+          <input
+            value={street}
+            onChange={({ target }) => dispatch({ type: 'street', payload: target.value })}
+          />
         </div>
         <div>
-          city <input value={city} onChange={({ target }) => setCity(target.value)} />
+          city
+          <input
+            value={city}
+            onChange={({ target }) => dispatch({ type: 'city', payload: target.value })}
+          />
         </div>
         <button type="submit">add!</button>
       </form>
