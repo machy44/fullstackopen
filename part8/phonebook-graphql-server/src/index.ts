186c2ccbe842fb571096aac8df99fd98ada45927
alen.sljivar <alen.sljivar@five.agency>
2022-10-03 09:55:05 +0200

    do setup with apollo-server-express

--- part8/phonebook-graphql-server/src/index.ts
+++ part8/phonebook-graphql-server/src/index.ts
@@ -1,7 +1,11 @@
 import mongoose from 'mongoose';
-import { ApolloServer } from 'apollo-server';
+import { ApolloServer } from 'apollo-server-express';
+import { ApolloServerPluginDrainHttpServer } from 'apollo-server-core';
+import { makeExecutableSchema } from '@graphql-tools/schema';
 import { MONGODB_URI } from './utils/config';
 import { info, error as errorLogger } from './utils/logger';
+import express from 'express';
+import http from 'http';
 
 import jwt, { JwtPayload as BaseJwtPayload } from 'jsonwebtoken';
 import { User } from './models/user';
@@ -25,19 +29,54 @@ mongoose
     errorLogger('error connecting to MongoDB:', error.message);
   });
 
-const server = new ApolloServer({
-  typeDefs, // graphql scheme
-  resolvers, // defined queries
-  context: async ({ req }) => {
-    const auth = req ? req.headers.authorization : null;
-    if (auth && auth.toLowerCase().startsWith('bearer ')) {
-      const decodedToken = jwt.verify(auth.substring(7), JWT_SECRET) as JwtPayload;
-      const currentUser = await User.findById(decodedToken.id).populate('friends');
-      return { currentUser };
-    }
-  },
-});
+// this is how you start with Apollo server
+// const server = new ApolloServer({
+//   typeDefs, // graphql scheme
+//   resolvers, // defined queries
+//   context: async ({ req }) => {
+//     const auth = req ? req.headers.authorization : null;
+//     if (auth && auth.toLowerCase().startsWith('bearer ')) {
+//       const decodedToken = jwt.verify(auth.substring(7), JWT_SECRET) as JwtPayload;
+//       const currentUser = await User.findById(decodedToken.id).populate('friends');
+//       return { currentUser };
+//     }
+//   },
+// });
 
-server.listen().then(({ url }) => {
-  console.log(`Server ready at ${url}`);
-});
+// server.listen().then(({ url }) => {
+//   console.log(`Server ready at ${url}`);
+// });
+
+// this is how you start with apollo-server-express
+
+const start = async () => {
+  const app = express();
+  const httpServer = http.createServer(app);
+
+  const schema = makeExecutableSchema({ typeDefs, resolvers });
+  const server = new ApolloServer({
+    schema,
+    context: async ({ req }) => {
+      const auth = req ? req.headers.authorization : null;
+      if (auth && auth.toLowerCase().startsWith('bearer ')) {
+        const decodedToken = jwt.verify(auth.substring(7), JWT_SECRET) as JwtPayload;
+        const currentUser = await User.findById(decodedToken.id).populate('friends');
+        return { currentUser };
+      }
+    },
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+  });
+
+  await server.start();
+
+  server.applyMiddleware({
+    app,
+    path: '/',
+  });
+
+  const PORT = 4000;
+
+  httpServer.listen(PORT, () => console.log(`Server is now running on http://localhost:${PORT}`));
+};
+
+start();
